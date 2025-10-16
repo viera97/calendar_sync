ebce9683203d050703ffc84ebf0236a4b48de690
Viera97 <d.viera1997@gmail.com>
2025-10-16 20:26:00 +0000

    feat: cambio a local

--- simple_api.py
+++ simple_api.py
@@ -292,4 +292,4 @@ async def root() -> Dict[str, Any]:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("simple_api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("simple_api:app", host="127.0.0.1", port=8000, reload=True)
